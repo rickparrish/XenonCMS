57df49d63c0ed65dcf0b523b999b0ccb5d54d33e
rickparrish <github@rick.parrish>
2015-08-03 20:34:41 -0400

    Load css/js from cdn (with local fallback)

--- XenonCMS/Scripts/xenoncms.js
+++ XenonCMS/Scripts/xenoncms.js
@@ -4,6 +4,12 @@
 }
 
 $(document).ready(function () {
+    // Check if the CDN loaded the css
+    if ($('#BootstrapCssTest').is(':visible') === true) {
+        $('#SiteTheme').attr('href', '/Content/bootstrap.' + SiteTheme + '.min.css');
+    }
+    
+    // Make errors appear more bootstrap-like
     $("span.field-validation-error").each(function (index) {
         $(this).parent().parent().addClass("has-error");
     });
