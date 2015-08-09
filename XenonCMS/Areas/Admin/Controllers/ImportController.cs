c2b429943226be6060d19529506f899cc8c63342
rickparrish <github@rick.parrish>
2015-08-08 22:22:54 -0400

    Posts and pages can now be updated on import

--- XenonCMS/Areas/Admin/Controllers/ImportController.cs
+++ XenonCMS/Areas/Admin/Controllers/ImportController.cs
@@ -60,50 +60,34 @@ namespace XenonCMS.Areas.Admin.Controllers
             foreach (var PostsFile in PostsFiles)
             {
                 string Slug = Path.GetFileNameWithoutExtension(PostsFile.Name);
-                var BlogPost = DB.SiteBlogPosts.SingleOrDefault(x => (x.Site.Domain == RequestDomain) && (x.Slug == Slug));
-                if (BlogPost == null)
+
+                // Open the file for reading
+                using (var InStream = PostsFile.Open())
                 {
-                    // Open the file for reading
-                    using (var InStream = PostsFile.Open())
+                    // Read each byte and convert to xml string
+                    var InBytes = new List<byte>();
+                    while (true)
                     {
-                        // Read each byte and convert to xml string
-                        var InBytes = new List<byte>();
-                        while (true)
-                        {
-                            var InByte = InStream.ReadByte();
-                            if (InByte == -1)
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                InBytes.Add((byte)InByte);
-                            }
-                        }
-                        var Xml = Encoding.UTF8.GetString(InBytes.ToArray());
-
-                        // Parse the xml string
-                        XmlDocument XmlDoc = new XmlDocument();
-                        XmlDoc.LoadXml(Xml);
-
-                        // Insert the blog posts
-                        // TODO "tags" element
-                        DB.SiteBlogPosts.Add(new SiteBlogPost()
-                        {
-                            DateLastUpdated = Convert.ToDateTime(XmlDoc.DocumentElement.SelectSingleNode("date").InnerText),
-                            DatePosted = Convert.ToDateTime(XmlDoc.DocumentElement.SelectSingleNode("date").InnerText),
-                            FullPostText = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("content").InnerText),
-                            SiteId = SiteId,
-                            Slug = Slug,
-                            Title = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("title").InnerText)
-                        });
-                        DB.SaveChanges();
-                        DatabaseCache.ResetBlogIndex(ControllerContext.RequestContext.HttpContext);
+                        var InByte = InStream.ReadByte();
+                        if (InByte == -1) break;
+                        InBytes.Add((byte)InByte);
                     }
-                }
-                else
-                {
-                    // TODO Skipping blog post, slug already exists
+
+                    // Parse the xml string
+                    XmlDocument XmlDoc = new XmlDocument();
+                    XmlDoc.LoadXml(Encoding.UTF8.GetString(InBytes.ToArray()));
+
+                    // Update existing or add new post
+                    var SBP = DB.SiteBlogPosts.SingleOrDefault(x => (x.Site.Domain == RequestDomain) && (x.Slug == Slug)) ?? new SiteBlogPost();
+                    SBP.DateLastUpdated = Convert.ToDateTime(XmlDoc.DocumentElement.SelectSingleNode("date").InnerText);
+                    SBP.DatePosted = Convert.ToDateTime(XmlDoc.DocumentElement.SelectSingleNode("date").InnerText);
+                    SBP.FullPostText = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("content").InnerText);
+                    SBP.SiteId = SiteId;
+                    SBP.Slug = Slug;
+                    SBP.Title = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("title").InnerText);
+                    if (SBP.Id <= 0) DB.SiteBlogPosts.Add(SBP);
+                    DB.SaveChanges();
+                    DatabaseCache.ResetBlogIndex(ControllerContext.RequestContext.HttpContext);
                 }
             }
         }
@@ -125,20 +109,13 @@ namespace XenonCMS.Areas.Admin.Controllers
                     while (true)
                     {
                         var InByte = InStream.ReadByte();
-                        if (InByte == -1)
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            InBytes.Add((byte)InByte);
-                        }
+                        if (InByte == -1) break;
+                        InBytes.Add((byte)InByte);
                     }
-                    var Xml = Encoding.UTF8.GetString(InBytes.ToArray());
 
                     // Parse the xml string
                     XmlDocument XmlDoc = new XmlDocument();
-                    XmlDoc.LoadXml(Xml);
+                    XmlDoc.LoadXml(Encoding.UTF8.GetString(InBytes.ToArray()));
 
                     // Check if the page exists TODO Test with a parent
                     string Parent = XmlDoc.DocumentElement.SelectSingleNode("parent").InnerText;
@@ -150,34 +127,29 @@ namespace XenonCMS.Areas.Admin.Controllers
                     }
                     else
                     {
-                        var Page = DB.SitePages.SingleOrDefault(x => (x.Site.Domain == RequestDomain) && (x.Slug == Slug));
-                        if (Page == null)
-                        {
-                            DB.SitePages.Add(new SitePage()
-                            {
-                                // TODO "meta" tag, 
-                                DateAdded = Convert.ToDateTime(XmlDoc.DocumentElement.SelectSingleNode("pubDate").InnerText),
-                                DateLastUpdated = Convert.ToDateTime(XmlDoc.DocumentElement.SelectSingleNode("pubDate").InnerText),
-                                DisplayOrder = Convert.ToInt32(XmlDoc.DocumentElement.SelectSingleNode("menuOrder").InnerText),
-                                Html = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("content").InnerText),
-                                Layout = "NormalNoSidebar", // TODO there is a "template" tag
-                                ParentId = 0, // TODO there is a "parent" tag
-                                RequireAdmin = false, // TODO "private" tag
-                                RightAlign = false,
-                                ShowInMenu = (XmlDoc.DocumentElement.SelectSingleNode("menuStatus").InnerText == "Y"),
-                                ShowTitleOnPage = true,
-                                SiteId = SiteId,
-                                Slug = Slug,
-                                Text = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("menu").InnerText), // TODO Rename text to something more intuitive
-                                Title = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("title").InnerText)
-                            });
-                            DB.SaveChanges();
-                            DatabaseCache.ResetNavMenuItems(ControllerContext.RequestContext.HttpContext);
-                        }
-                        else
-                        {
-                            // TODO Skipping page, slug already exists
-                        }
+                        if (Slug == "index") Slug = "home";
+
+                        // Update existing or add new page
+                        var SP = DB.SitePages.SingleOrDefault(x => (x.Site.Domain == RequestDomain) && (x.Slug == Slug)) ?? new SitePage();
+                        // TODO "meta" tag, 
+                        SP.DateAdded = Convert.ToDateTime(XmlDoc.DocumentElement.SelectSingleNode("pubDate").InnerText);
+                        SP.DateLastUpdated = Convert.ToDateTime(XmlDoc.DocumentElement.SelectSingleNode("pubDate").InnerText);
+                        SP.DisplayOrder = Convert.ToInt32(XmlDoc.DocumentElement.SelectSingleNode("menuOrder").InnerText);
+                        SP.Html = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("content").InnerText);
+                        SP.Layout = "NormalNoSidebar"; // TODO there is a "template" tag
+                        SP.ParentId = 0; // TODO there is a "parent" tag
+                        SP.RequireAdmin = false; // TODO "private" tag
+                        SP.RightAlign = false;
+                        SP.ShowInMenu = (XmlDoc.DocumentElement.SelectSingleNode("menuStatus").InnerText == "Y");
+                        SP.ShowTitleOnPage = true;
+                        SP.SiteId = SiteId;
+                        SP.Slug = Slug;
+                        SP.Text = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("menu").InnerText); // TODO Rename text to something more intuitive
+                        SP.Title = HttpUtility.HtmlDecode(XmlDoc.DocumentElement.SelectSingleNode("title").InnerText);
+                        if (SP.Id <= 0) DB.SitePages.Add(SP);
+                        DB.SaveChanges();
+                        DatabaseCache.ResetNavMenuItems(ControllerContext.RequestContext.HttpContext);
+                        DatabaseCache.RemoveSitePage(ControllerContext.RequestContext.HttpContext, Slug);
                     }
                 }
             }
