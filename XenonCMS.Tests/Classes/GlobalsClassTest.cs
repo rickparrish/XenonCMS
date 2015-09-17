7a7cf64f1acd59931097d037fb49ff83ade1042b
rickparrish <github@rick.parrish>
2015-09-17 15:28:25 -0400

    Should really learn this TDD stuff

---
+++ XenonCMS.Tests/Classes/GlobalsClassTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web;
+
+namespace XenonCMS.Tests.Classes
+{
+    [TestClass]
+    public class GlobalsClassTest
+    {
+        [TestMethod]
+        public void TestGetRequestDomain()
+        {
+            var HCB = new HttpContextWrapper(
+                new HttpContext(
+                    new HttpRequest(null, "http://www.RickParrish.ca", null),
+                    new HttpResponse(null)
+                )
+            );
+            string Domain = XenonCMS.Classes.Globals.GetRequestDomain(HCB);
+            Assert.AreEqual("rickparrish.ca", Domain);
+
+            HCB = new HttpContextWrapper(
+                new HttpContext(
+                    new HttpRequest(null, "http://RickParrish.ca", null),
+                    new HttpResponse(null)
+                )
+            );
+            Domain = XenonCMS.Classes.Globals.GetRequestDomain(HCB);
+            Assert.AreEqual("rickparrish.ca", Domain);
+
+            HCB = new HttpContextWrapper(
+                new HttpContext(
+                    new HttpRequest(null, "http://www.a.b.c.d.RickParrish.ca", null),
+                    new HttpResponse(null)
+                )
+            );
+            Domain = XenonCMS.Classes.Globals.GetRequestDomain(HCB);
+            Assert.AreEqual("a.b.c.d.rickparrish.ca", Domain);
+
+            HCB = new HttpContextWrapper(
+                new HttpContext(
+                    new HttpRequest(null, "http://a.b.c.d.RickParrish.ca", null),
+                    new HttpResponse(null)
+                )
+            );
+            Domain = XenonCMS.Classes.Globals.GetRequestDomain(HCB);
+            Assert.AreEqual("a.b.c.d.rickparrish.ca", Domain);
+        }
+    }
+}
